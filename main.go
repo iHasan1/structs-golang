1c96e0b6002e6e9eb888a7a22f74d8faffd8b2b2
Imran Hasan <imran.h94@gmail.com>
2023-09-18 12:32:05 +0500

    resolved by value by using pointers

--- main.go
+++ main.go
@@ -23,15 +23,13 @@ func main() {
 		},
 	}
 
-	jim.print()
-
-	jim.updateName("Jimmy")
-
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
 	jim.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
